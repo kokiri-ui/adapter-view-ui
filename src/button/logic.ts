a8ac9a1a4bef216949fd057f97c4824b1b7dc433
Ourai Lin <ourairyu@gmail.com>
2021-11-26 06:11:07 +0800

    chore: move files

--- src/button/logic.ts
+++ src/button/logic.ts
@@ -1,44 +1,25 @@
 import { includes } from '@ntks/toolbox';
-import {
-  ButtonBorder,
-  ButtonSize,
-  IButtonComponent,
-  ButtonHeadlessComponent,
-} from 'petals-ui/dist/button';
+
 import { CreateElement, VNode } from 'vue';
-import { Component, Prop, Emit } from 'vue-property-decorator';
+import { Component } from 'vue-property-decorator';
+
+import { ButtonStructuralComponent } from '@kokiri/core/dist/button';
 import { Button as IvuButton } from 'view-design';
 
-import { getComponentName, BaseStructuralComponent } from '../basic';
+import { getComponentName } from '../basic';
 
 @Component({
   // @ts-ignore
   abstract: true,
   name: getComponentName('button'),
 })
-export default class Button
-  extends BaseStructuralComponent<ButtonHeadlessComponent>
-  implements IButtonComponent {
-  @Prop({ type: String, default: 'medium' })
-  public readonly size!: ButtonSize;
-
-  @Prop({ type: String, default: 'solid' })
-  public readonly border!: ButtonBorder;
-
-  @Prop({ type: Boolean, default: false })
-  public readonly outlined!: boolean;
-
-  @Prop({ type: String, default: '' })
-  public readonly color!: string;
-
-  @Prop({ type: Boolean, default: false })
-  public readonly disabled!: boolean;
-
-  @Emit('click')
-  private handleClick(): void {} // eslint-disable-line @typescript-eslint/no-empty-function
-
-  public render(h: CreateElement): VNode {
-    const props: Record<string, any> = { disabled: this.disabled, ghost: this.outlined };
+export default class Button extends ButtonStructuralComponent {
+  private render(h: CreateElement): VNode {
+    const props: Record<string, any> = {
+      disabled: this.disabled,
+      ghost: this.outlined,
+      htmlType: this.nativeType,
+    };
 
     if (includes(this.color, ['primary', 'success', 'warning', 'danger', 'info'])) {
       props.type = this.color === 'danger' ? 'error' : this.color;
@@ -54,10 +35,10 @@ export default class Button
       props.size = this.size;
     }
 
-    return h(IvuButton, { props, on: { click: this.handleClick } }, this.$slots.default);
-  }
-
-  public created(): void {
-    this.setHeadlessComponent(new ButtonHeadlessComponent(this));
+    return h(
+      IvuButton,
+      { class: this.className, props, on: { click: this.onClick } },
+      this.$slots.default,
+    );
   }
 }
--- src/text-input/logic.ts
+++ src/text-input/logic.ts
@@ -1,47 +1,18 @@
-import { FormControlSize } from 'petals-ui/dist/form-control';
-import { ITextInputComponent, TextInputHeadlessComponent } from 'petals-ui/dist/text-input';
-import { CreateElement, VNode } from 'vue';
-import { Component, Prop } from 'vue-property-decorator';
+import { Component } from 'vue-property-decorator';
+
+import { TextInputStructuralComponent } from '@kokiri/core/dist/text-input';
 import { Input as IvuInput } from 'view-design';
 
-import { getComponentName, BaseStructuralComponent } from '../basic';
+import { getComponentName } from '../basic';
 
 @Component({
   // @ts-ignore
   abstract: true,
   name: getComponentName('textInput'),
+  components: { IvuInput },
 })
-export default class TextInput
-  extends BaseStructuralComponent<TextInputHeadlessComponent>
-  implements ITextInputComponent {
-  @Prop({ type: String, default: '' })
-  public readonly name!: string;
-
-  @Prop({ type: String, default: '' })
-  public readonly value!: string;
-
-  @Prop({ type: Boolean, default: false })
-  public readonly disabled!: boolean;
-
-  @Prop({ type: Boolean, default: false })
-  public readonly readonly!: boolean;
-
-  @Prop({ type: String, default: '' })
-  public readonly placeholder!: string;
-
-  @Prop({ type: Boolean, default: false })
-  public readonly clearable!: boolean;
-
-  @Prop({ type: String, default: 'medium' })
-  public readonly size!: FormControlSize;
-
-  @Prop({ type: Number })
-  public readonly minLength!: number;
-
-  @Prop({ type: Number })
-  public readonly maxLength!: number;
-
-  public render(h: CreateElement): VNode {
+export default class TextInput extends TextInputStructuralComponent {
+  private get resolvedProps(): Record<string, any> {
     const props: Record<string, any> = {
       name: this.name,
       value: this.value,
@@ -63,10 +34,6 @@ export default class TextInput
       props.minlength = this.minLength;
     }
 
-    return h(IvuInput, { props });
-  }
-
-  public created(): void {
-    this.setHeadlessComponent(new TextInputHeadlessComponent(this));
+    return props;
   }
 }
